978664b2fbb752dfb9478c9adde903b7a9598d6f
Alic <156869760+aliciaxl@users.noreply.github.com>
2024-05-06 10:59:02 -0700

    updated grid to change background color to randomized color

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const container = document.querySelector("#container");
-let rowNum = 16;
-let columnNum = rowNum;
+let rowNum;
+const columnNum = rowNum;
 
-let makeGrid = () => {
+function random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+
+let makeGrid = (rowNum) => {
     for (let i = 0; i < rowNum; i++) {
     const rowCells = document.createElement("div");
-    rowCells.id = "rowCells";
+    rowCells.className = "rowCells";
     container.appendChild(rowCells);
+    const columnNum = rowNum;
         for (let i = 0; i < columnNum; i++) {  
             const columnCells = document.createElement("div");
-            columnCells.id = "columnCells";
+            columnCells.className = "columnCells";
             rowCells.appendChild(columnCells);
+            columnCells.addEventListener ('mouseover', () => {
+            const randomColor = `rgb(${random(50, 255)}, ${random(50, 255)}, ${random(50, 255)})`
+                columnCells.style.backgroundColor = randomColor;
+            })
       } 
     }
 };
-makeGrid();
+makeGrid(16);
 
-document.querySelector("#numofSquares").onclick = function() {
-    let numSquares = prompt("How many squares per side?");
-    container.remove();
-    let rowNum = numSquares;
-    makeGrid();
+document.querySelector("#numofSquares").onclick = () => {
+    container.innerHTML = ''
+    let rowNum = prompt("How many squares per side? (1-100)");
+makeGrid(rowNum);
 };
